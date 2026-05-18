01047ff8ace414f6d659b8dcfb94a0c25dfe4837
MD Riajuddin Rumman <143687344+SnowmassX@users.noreply.github.com>
2026-05-18 18:04:44 +0600

    login done
    with remember me

---
+++ controller/loginCheck.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require_once('../model/userModel.php');
+
+if (isset($_POST['submit'])) {
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
+
+    $userData = getUserByEmail($email);
+
+    if ($userData && password_verify($password, $userData['password_hash'])) {
+
+        $_SESSION['id'] = $userData['id'];
+        $_SESSION['name'] = $userData['name'];
+        $_SESSION['role'] = $userData['role'];
+
+        if (isset($_POST['remember'])) {
+            setcookie('email', $email, time() + (86400 * 30), "/");
+            setcookie('password', $password, time() + (86400 * 30), "/");
+        } else {
+            setcookie('email', '', time() - 3600, "/");
+            setcookie('password', '', time() - 3600, "/");
+        }
+
+        if ($userData['role'] == "Admin") {
+            header('location: ../view/admin/dashboard.php');
+        } else {
+            header('location: ../view/home.php');
+        }
+        exit();
+    } else {
+        echo "Invalid email or password.";
+    }
+}
